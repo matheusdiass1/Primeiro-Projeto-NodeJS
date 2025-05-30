08bb486d49461323b3194f7c683fa4400a0820cf
matheusdiass1 <matheus_oliveiradias@hotmail.com>
2025-05-30 12:39:52 -0300

    ajuste mas rotas get e put

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client';
-
+import cors from 'cors'
 
 
 const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 
 app.get('/usuarios', async (req, res) => {
